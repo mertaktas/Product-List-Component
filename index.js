a2a74a13b5a0b7a2ebd835a026549c2b3c66f679
mertaktas <mertaktas231@gmail.com>
2021-08-25 19:07:13 +0300

    category data call

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ function getCategories(datas,html){
     NavbarListEls.innerHTML = ""
     for (let data of datas) {
         let li = document.createElement("li");
-        li.classList.add('p-3', 'text-sm', 'text-gray-400', 'h-auto', 'font-medium', 'cursor-pointer', 'lg:ml-4', 'truncate')
-        li.innerHTML = `<span>${data}</span>`
         
+        li.classList.add('p-3', 'text-sm', 'text-gray-400', 'h-auto', 'font-medium', 'cursor-pointer', 'lg:ml-4','lg:truncate','break-all')
+        li.innerHTML += `<span>${data}</span>`
         html.appendChild(li)
     }
+    html.firstChild.classList.add('text-gray-700', 'border-b-2', 'border-blue-400', 'lg:bg-green-100', 'lg:bg-green-50', 'lg:text-blue-700', 'lg:border-b-0')
+    let span = document.createElement("span");
+    span.classList.add('h-full', 'w-1', 'bg-blue-700', 'absolute', 'rounded-r-sm', '-left-4')
+    html.firstChild.appendChild(span)
 }
