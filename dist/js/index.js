d724da6c1dcc7ec88d2e8c4764c98a9bf26392b6
mertaktas <mertaktas231@gmail.com>
2021-08-26 23:48:05 +0300

    Updated loading cart

--- dist/js/index.js
+++ dist/js/index.js
@@ -80,18 +80,18 @@ function loadingCart(){
         ProductListEl.innerHTML += `
         <li class="border border-gray-300 shadow rounded-md p-2 max-w-sm w-full mx-auto min-w-40 mr-2">
             <div class="animate-pulse flex flex-col">
-            <div class="rounded-lg bg-gray-400 h-36 w-full"></div>
-            <div class="flex-1 space-y-4 py-1">
-                <div class="space-y-1 mt-4">
-                    <div class="h-3 bg-gray-400 rounded-lg w-full"></div>
-                    <div class="h-3 bg-gray-400 rounded-lg w-full"></div>
+                <div class="rounded-lg bg-gray-400 h-40 w-full"></div>
+                <div class="flex-1 space-y-4 py-1">
+                    <div class="space-y-1 mt-4 px-2">
+                        <div class="h-3 bg-gray-400 rounded-lg w-full"></div>
+                        <div class="h-3 bg-gray-400 rounded-lg w-full"></div>
+                    </div>
+                    <div class="space-y-3">
+                    <div class="h-10 mt-6 bg-gray-400 rounded-lg w-full flex items-center"></div>
+                    <div class="h-3 bg-gray-400 rounded-lg w-3/5"></div>
+                    <div class="h-10 bg-gray-400 rounded-lg w-full"></div>
+                    </div>
                 </div>
-                <div class="space-y-2">
-                <div class="h-10 mt-6 bg-gray-400 rounded-lg w-full flex items-center"></div>
-                <div class="h-4 bg-gray-400 rounded-lg w-3/5"></div>
-                <div class="h-10 bg-gray-400 rounded-lg w-full"></div>
-                </div>
-            </div>
             </div>
         </li>
     `
