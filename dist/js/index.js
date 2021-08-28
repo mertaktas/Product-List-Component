cb10d5b3401b48c7fcc18320a6604cdaa73fe7e8
mertaktas <mertaktas231@gmail.com>
2021-08-28 14:40:11 +0300

    Lazy load

--- dist/js/index.js
+++ dist/js/index.js
@@ -64,44 +64,20 @@ function getProducts(datas) {
         datas.map(data=>{
             ProductListEl.innerHTML += strProduct(data)
         }) 
-        function logElementEvent(eventName, element) {
-          console.log(Date.now(), eventName, element.getAttribute("data-src"));
-        }
-    
-        var callback_enter = function (element) {
-          logElementEvent("🔑 ENTERED", element);
-        };
-        var callback_exit = function (element) {
-          logElementEvent("🚪 EXITED", element);
-        };
-        var callback_loading = function (element) {
-          logElementEvent("⌚ LOADING", element);
-        };
-        var callback_loaded = function (element) {
-          logElementEvent("👍 LOADED", element);
-        };
-        var callback_error = function (element) {
-          logElementEvent("💀 ERROR", element);
-          element.src =
-            "https://via.placeholder.com/440x560/?text=Error+Placeholder";
-        };
-        var callback_finish = function () {
-          logElementEvent("✔️ FINISHED", document.documentElement);
-        };
-        var callback_cancel = function (element) {
-          logElementEvent("🔥 CANCEL", element);
-        };
-        
-        var ll = new LazyLoad({
-          rootMargins: "0px",
-          // Assign the callbacks defined above
-          callback_enter: callback_enter,
-          callback_exit: callback_exit,
-          callback_cancel: callback_cancel,
-          callback_loading: callback_loading,
-          callback_loaded: callback_loaded,
-          callback_error: callback_error,
-          callback_finish: callback_finish
+        // Lazy Load
+        const images = document.querySelectorAll("img");
+        const imgOptions = {};
+        const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+        entries.forEach((entry) => {
+            if (!entry.isIntersecting) return;
+
+            const img = entry.target;
+            img.src = img.dataset.src
+            imgObserver.unobserve(entry.target);
+        });
+        }, imgOptions);
+        images.forEach((img) => {
+        imgObserver.observe(img);
         });
     }, 300);
     
